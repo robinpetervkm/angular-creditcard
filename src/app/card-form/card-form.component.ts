97a3418076fb0918584bc2a68c148ce590f78590
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-15 18:42:10 -0300

    MinLength validator there are multip validators

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -8,7 +8,10 @@ import { FormGroup,FormControl, Validator, Validators } from '@angular/forms';
 })
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('',[Validators.required])
+    name: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3)
+    ])
   });
 
   constructor() { }
