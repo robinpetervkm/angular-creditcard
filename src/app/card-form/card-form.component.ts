da49d19511ff60e2c586c03d4e603adfd4b306d3
Robin Peter <robin.peter.vkm@gmail.com>
2021-03-15 18:34:10 -0300

    Validators will inforce rules

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
