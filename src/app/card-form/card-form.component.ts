c480ada4723d398b39427e3ae175c70f22928f70
Robin Peter <robin.peter.vkm@gmail.com>
2021-05-26 14:08:25 -0300

    new validation is added as value entred not more than 5 charaters

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl, Validator, Validators } from '@angular/forms';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -10,7 +10,8 @@ export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('',[
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      Validators.maxLength(5)
     ])
   });
 
