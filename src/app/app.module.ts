0f40b27e4c2a1f4a9ad03c98c2415e87dd80e5d9
Robin Peter <robin.peter.vkm@gmail.com>
2021-05-27 17:22:53 -0300

    included in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { ReactiveFormsModule } from  '@angular/forms/'
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
+
 
 @NgModule({
   declarations: [
@@ -14,6 +18,7 @@ import { InputComponent } from './input/input.component';
   ],
   imports: [
     BrowserModule,
+    NgxMaskModule.forRoot(),
     ReactiveFormsModule
   ],
   providers: [],
