177e3aacc46548db37ffe3e1b39f1c8e1bf8fe60
Robin Peter <robin.peter.vkm@gmail.com>
2021-05-27 17:14:39 -0300

    implemented our own logic

--- src/app/date-from-control.ts
+++ src/app/date-from-control.ts
@@ -1,6 +1,18 @@
 import { FormControl } from '@angular/forms';
 export class DateFromControl extends FormControl {
   setValue(value: String, options: any) {
+    if (value.match(/[^0-9|\/]/gi)) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
     if (value.length === 2) {
       super.setValue(value + '/', { ...options, emitModelToViewChange: true });
       return;
