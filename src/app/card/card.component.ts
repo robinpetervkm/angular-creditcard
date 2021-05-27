3d078e147f82f3299f99b95fc34d200382d1191e
Robin Peter <robin.peter.vkm@gmail.com>
2021-05-27 18:40:54 -0300

    Created the Input Param in card comp

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input() cardNumber: String;
+  @Input() name: String;
+  @Input() expiration: String;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
