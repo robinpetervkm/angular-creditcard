54aecc76005779308e03c1ed7bad1de4a286a82e
Robin Peter <robin.peter.vkm@gmail.com>
2021-05-27 13:03:05 -0300

    delete the inserted char is not possible because it is added just back when the char length become 2

--- src/app/date-from-control.ts
+++ src/app/date-from-control.ts
@@ -1,7 +1,10 @@
 import { FormControl } from '@angular/forms';
 export class DateFromControl extends FormControl {
   setValue(value: String, options: any) {
-    console.log(value);
-    super.setValue(value + '*', { ...options, emitModelToViewChange: true });
+    if (value.length === 2) {
+      super.setValue(value + '/', { ...options, emitModelToViewChange: true });
+      return;
+    }
+    super.setValue(value, { ...options, emitModelToViewChange: true });
   }
 }
