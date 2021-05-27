5a2366b7d153e98dcd1c3a89b10aceb7315f0098
Robin Peter <robin.peter.vkm@gmail.com>
2021-05-27 12:50:00 -0300

    value is pass to the fromGroup with the help of super.setValue functiob of the original setValue function of the FormControl

--- src/app/date-from-control.ts
+++ src/app/date-from-control.ts
@@ -2,5 +2,6 @@ import { FormControl } from '@angular/forms';
 export class DateFromControl extends FormControl {
   setValue(value: String, options: any) {
     console.log(value);
+    super.setValue(value,options);
   }
 }
